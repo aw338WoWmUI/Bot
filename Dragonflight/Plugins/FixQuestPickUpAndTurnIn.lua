2e737e8661a038dd135926525cb3fc88a57a27e9
Aki Konani <aki.konani@gmail.com>
2022-10-29 23:42:26 +0100

    Support for mass pick up

--- Dragonflight/Plugins/FixQuestPickUpAndTurnIn.lua
+++ Dragonflight/Plugins/FixQuestPickUpAndTurnIn.lua
@@ -1,21 +1,18 @@
 local function onGossipShow()
-  print('onGossipShow')
   local questID = GMR.GetQuestId()
-  if questID then
-    local availableQuests = C_GossipInfo.GetAvailableQuests()
-    local availableQuest = Array.find(availableQuests, function(quest)
+  local availableQuests = C_GossipInfo.GetAvailableQuests()
+  local availableQuest = Array.find(availableQuests, function(quest)
+    return (questID and quest.questID == questID) or GMR.IsMassQuestId(quest.questID)
+  end)
+  if availableQuest then
+    C_GossipInfo.SelectAvailableQuest(availableQuest.questID)
+  elseif questID then
+    local activeQuests = C_GossipInfo.GetActiveQuests()
+    local activeQuest = Array.find(activeQuests, function(quest)
       return quest.questID == questID
     end)
-    if availableQuest then
-      C_GossipInfo.SelectAvailableQuest(availableQuest.questID)
-    else
-      local activeQuests = C_GossipInfo.GetActiveQuests()
-      local activeQuest = Array.find(activeQuests, function(quest)
-        return quest.questID == questID
-      end)
-      if activeQuest then
-        C_GossipInfo.SelectActiveQuest(activeQuest.questID)
-      end
+    if activeQuest then
+      C_GossipInfo.SelectActiveQuest(activeQuest.questID)
     end
   end
 end
