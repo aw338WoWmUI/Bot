7e23d0abcabadc9a75ac79cabec95f6923af0f6c
Aki Konani <aki.konani@gmail.com>
2022-10-29 17:56:25 +0100

    Fixes typo

--- Dragonflight/Plugins/Fixes.lua
+++ Dragonflight/Plugins/Fixes.lua
@@ -2,7 +2,7 @@ local ticker
 ticker = C_Timer.NewTicker(0, function()
   if _G.GMR then
     ticker:Cancel()
-    function GMR.IsBattleGroundChecked()
+    function GMR.IsBattlegroundChecked()
       return false
     end
   end
