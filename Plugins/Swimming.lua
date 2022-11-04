1166c0ff675efa726deedaa1043a91899076e19c
Aki Konani <aki.konani@gmail.com>
2022-11-04 07:19:33 +0000

    Fix for swimming

---
+++ Plugins/Swimming.lua
@@ -0,0 +1,28 @@
+local function calculateDistance(a, b)
+  return math.sqrt(
+    math.pow(b.x - a.x, 2) +
+      math.pow(b.y - a.y, 2)
+  )
+end
+
+local faceSmoothly = GMR.FaceSmoothly
+GMR.FaceSmoothly = function(x, y, z)
+  local playerPosition = GMR.GetPlayerPosition()
+  if GMR.IsPositionUnderwater(playerPosition.x, playerPosition.y, playerPosition.z) then
+    local distance = calculateDistance(playerPosition, { x = x, y = y })
+
+    if distance > 0.5 then
+      GMR.FaceDirection(x, y, z)
+    end
+
+    if distance < 0.5 then
+      if z < playerPosition.z then
+        GMR.SetPitch(-0.5 * PI)
+      elseif z > playerPosition.z then
+        GMR.SetPitch(0.5 * PI)
+      end
+    end
+  else
+    faceSmoothly(x, y, z)
+  end
+end
