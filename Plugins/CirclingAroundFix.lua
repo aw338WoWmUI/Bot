a1e5f0109431d3db2dea897f9b05bd5666d9811f
Aki Konani <aki.konani@gmail.com>
2022-11-17 12:26:55 +0000

    Work

---
+++ Plugins/CirclingAroundFix.lua
@@ -0,0 +1,30 @@
+local function calculateDistance(a, b)
+  return math.sqrt(
+    math.pow(b.x - a.x, 2) +
+      math.pow(b.y - a.y, 2)
+  )
+end
+
+local faceSmoothly = GMR.FaceSmoothly
+GMR.FaceSmoothly = function(...)
+  local args = { ... }
+  local x, y, z
+  if type(args[1]) == 'string' then
+    x, y, z = GMR.ObjectPosition(x)
+  else
+    x, y, z = args[1], args[2], args[3]
+  end
+
+  if x and y and z and not GMR.IsFacingXYZ(x, y, z) then
+    local playerPosition = GMR.GetPlayerPosition()
+
+    local distance = calculateDistance(playerPosition, { x = x, y = y })
+
+    if distance <= 5 then
+      GMR.FaceDirection(x, y, z)
+      return
+    end
+  end
+
+  return faceSmoothly(...)
+end
