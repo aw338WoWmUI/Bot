d566eb2b7d5649b73749fe3fb462ec222cf911e4
Aki Konani <aki.konani@gmail.com>
2022-12-17 17:47:28 +0100

    Fishing: only use buff item when buff item is available

--- AddOns/Fishing/Fishing.lua
+++ AddOns/Fishing/Fishing.lua
@@ -6,6 +6,9 @@ local _ = {}
 
 local FISHING_SPELL_ID = 131474
 local FISHING_BOBBER_OBJECT_ID = 35591
+local NIGHTCRAWLERS_ITEM_ID = 6530
+local CAPTAIN_RUMSEYS_LAGER_ITEM_ID = 34832
+local SCALEBELLY_MACKEREL_ITEM_ID = 194730
 local MAXIMUM_FISHING_DURATION = 30 -- seconds
 
 local isFishing = false
@@ -19,21 +22,21 @@ function Fishing.toggleFishing()
   else
     isFishing = true
 
-    exitTimer = C_Timer.NewTimer(_.hours(3), function ()
+    exitTimer = C_Timer.NewTimer(_.hours(3), function()
       Exit()
     end)
 
     Coroutine.runAsCoroutine(function()
       while true do
-        if not _.isFishingPoleEnchantedWithFishingLure() then
+        if _.hasFishingLure() and not _.isFishingPoleEnchantedWithFishingLure() then
           _.enchantFishingPoleWithFishingLure()
         end
 
-        if _.isCaptainRumseysLagerBuffDurationShorterThanMaximumFishingDuration() then
+        if _.hasCaptainRumseysLager() and _.isCaptainRumseysLagerBuffDurationShorterThanMaximumFishingDuration() then
           _.buffWithCaptainRumseysLagerBuff()
         end
 
-        if _.isChumBuffDurationShorterThanMaximumFishingDuration() then
+        if _.hasScalebellyMackerel() and _.isChumBuffDurationShorterThanMaximumFishingDuration() then
           _.buffWithChumBuff()
         end
 
@@ -74,12 +77,12 @@ function _.hours(amount)
   return amount * 60 * 60
 end
 
-function _.hasFishingLures()
-
+function _.hasFishingLure()
+  return Boolean.toBoolean(_.hasItem(NIGHTCRAWLERS_ITEM_ID))
 end
 
 function _.isFishingPoleEnchantedWithFishingLure()
-	local tooltip = C_TooltipInfo.GetInventoryItem('player', 28, false)
+  local tooltip = C_TooltipInfo.GetInventoryItem('player', 28, false)
   return Array.any(tooltip.lines, _.isFishingLureEnchantmentLine)
 end
 
@@ -92,9 +95,13 @@ function _.enchantFishingPoleWithFishingLure()
   -- C_Container.UseContainerItem
   UseItemByName('Nightcrawlers')
   PickupInventoryItem(28)
-  Events.waitForEventCondition('UNIT_SPELLCAST_SUCCEEDED', function (self, event, unit)
+  Events.waitForEventCondition('UNIT_SPELLCAST_SUCCEEDED', function(self, event, unit)
     return unit == 'player'
   end)
+end
+
+function _.hasCaptainRumseysLager()
+  return Boolean.toBoolean(_.hasItem(CAPTAIN_RUMSEYS_LAGER_ITEM_ID))
 end
 
 function _.isCaptainRumseysLagerBuffDurationShorterThanMaximumFishingDuration()
@@ -103,6 +110,10 @@ end
 
 function _.buffWithCaptainRumseysLagerBuff()
   _.useBuffItem("Captain Rumsey's Lager")
+end
+
+function _.hasScalebellyMackerel()
+  return Boolean.toBoolean(_.hasItem(SCALEBELLY_MACKEREL_ITEM_ID))
 end
 
 function _.isChumBuffDurationShorterThanMaximumFishingDuration()
@@ -114,14 +125,27 @@ function _.buffWithChumBuff()
 end
 
 function _.isBuffDurationShorterThanMaximumFishingDuration(buffName)
-	local expirationTime = select(6, AuraUtil.FindAuraByName(buffName, 'player'))
+  local expirationTime = select(6, AuraUtil.FindAuraByName(buffName, 'player'))
   return Boolean.toBoolean(not expirationTime or expirationTime - GetTime() < MAXIMUM_FISHING_DURATION)
 end
 
 function _.useBuffItem(itemName)
-	UseItemByName(itemName)
+  UseItemByName(itemName)
   local waitDuration = _.randomFloat(0.5, 1)
   Coroutine.waitForDuration(waitDuration)
+end
+
+function _.hasItem(itemID)
+  for containerIndex = 0, NUM_BAG_SLOTS do
+    for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
+      local itemInfo = Compatibility.Container.retrieveItemInfo(containerIndex, slotIndex)
+      if itemInfo and itemInfo.itemID == itemID then
+        return true
+      end
+    end
+  end
+
+  return false
 end
 
 function _.randomFloat(from, to)
