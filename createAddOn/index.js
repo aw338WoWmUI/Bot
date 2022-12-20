d60d776205fbfe9739b60f5bfb49574bca0f84c9
Aki Konani <aki.konani@gmail.com>
2022-12-20 19:06:20 +0100

    Automatically add symlink creation statements to the "create_symbolic_links" scripts when creating an add-on

--- createAddOn/index.js
+++ createAddOn/index.js
@@ -58,3 +58,43 @@ local _ = {}
   const content = await readFile(join(directoryPath, '..', 'LICENSE'))
   await writeFile(join(addOnPath, 'LICENSE'), content)
 }
+
+{
+  const createSymbolicLinksScriptPaths = [
+    join(directoryPath, '..', 'create_symbolic_links.template.bat'),
+    join(directoryPath, '..', 'create_symbolic_links.bat')
+  ]
+  for (const createSymbolicLinksScriptPath of createSymbolicLinksScriptPaths) {
+    let content
+    try {
+      content = await readFile(createSymbolicLinksScriptPath)
+    } catch (error) {
+      if (error.code == 'ENOENT') {
+        continue
+      } else {
+        throw error
+      }
+    }
+    const regExp = /if exist "%path%\\_(.+?)_" \(\n  if not exist "%path%\\_.+?_\\Interface" mkdir "%path%\\_.+?_\\Interface"\n  if not exist "%path%\\_.+?_\\Interface\\AddOns" mkdir "%path%\\_.+?_\\Interface\\AddOns"\n(.*?)\)/sdg
+    let match
+    while (match = regExp.exec(content)) {
+      const version = match[1]
+      let mklinkStatements = match[2].split('\n')
+      mklinkStatements = mklinkStatements.slice(0, mklinkStatements.length - 1)
+      const addOnNameRegExp = /mklink \/D "%path%\\_.+?_\\Interface\\AddOns\\(.+?)"/
+      const addOnNames = mklinkStatements.map(statement => addOnNameRegExp.exec(statement)[1])
+      const addOnNameLowerCase = addOnName.toLowerCase()
+      const index = addOnNames.findIndex(addOnName2 => addOnNameLowerCase < addOnName2.toLowerCase())
+      mklinkStatements.splice(
+        index,
+        0,
+        `  mklink /D "%path%\\_${ version }_\\Interface\\AddOns\\${ addOnName }" "%~dp0\\AddOns\\${ addOnName }"`
+      )
+      content = content.substr(0, match.indices[2][0]) +
+        mklinkStatements.join('\n') +
+        '\n' +
+        content.substr(match.indices[2][1])
+    }
+    await writeFile(createSymbolicLinksScriptPath, content)
+  }
+}
